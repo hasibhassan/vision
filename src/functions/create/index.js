84ca48644afa663423f5db2094464b82cac95037
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-18 16:26:06 -0500

    fix: api create route

--- src/functions/create/index.js
+++ src/functions/create/index.js
@@ -4,7 +4,7 @@ const db = new AWS.DynamoDB.DocumentClient()
 async function createData(id, data) {
   const params = {
     TableName: process.env.TABLE_NAME,
-    Item: { id },
+    Item: { id, data },
   }
   try {
     await db.put(params).promise()
@@ -19,7 +19,7 @@ export default async (event, context) => {
     console.log('context id is:', contextId)
     const { data } = event.body
     console.log('the data is ...', data)
-    await createData(contextId)
+    await createData(contextId, data)
     return `item ${contextId} successfully created with value ${data}`
   } catch (err) {
     return { error: err }
