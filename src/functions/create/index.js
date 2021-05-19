411dc0e84809a6c46cb9b751f291af58cf96f498
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-19 15:20:43 -0500

    fix: parse event body

--- src/functions/create/index.js
+++ src/functions/create/index.js
@@ -18,7 +18,7 @@ async function createItem(id, data) {
 export default async (event, context) => {
   try {
     const contextId = context.awsRequestId
-    const { data } = JSON.stringify(event.body)
+    const { data } = JSON.parse(event.body)
     let returnedData = await createItem(contextId, data)
     returnedData = JSON.stringify(returnedData)
     return `the returned values are: ${returnedData}`
