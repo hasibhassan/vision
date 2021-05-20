5d17f7fea0189b3111cfabb06365c221e81a28a0
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-20 16:04:40 -0500

    fix: get lambda return body

--- src/functions/get/index.js
+++ src/functions/get/index.js
@@ -1,27 +1,25 @@
 import AWS from 'aws-sdk'
 const db = new AWS.DynamoDB.DocumentClient()
 
-async function getItem(id) {
-  const params = {
-    TableName: process.env.TABLE_NAME,
-    Key: { id },
-  }
-  try {
-    await db.get(params).promise()
-  } catch (err) {
-    return err
-  }
-}
+// async function getItem(id) {
+//   const params = {
+//     TableName: process.env.TABLE_NAME,
+//     Key: { id },
+//   }
+//   try {
+//     await db.get(params).promise()
+//   } catch (err) {
+//     return err
+//   }
+// }
 
 export default async (event) => {
   const id = event.pathParameters.id
 
   try {
-    let result = await getItem(id)
+    let body = await getItem(id)
     console.log(`log is: ${result}`)
-    result = JSON.stringify(result)
-    console.log(`log after stringifying it is: ${result}`)
-    return result
+    return body
   } catch (err) {
     return { error: err }
   }
