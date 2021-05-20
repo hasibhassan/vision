0c24a91d76b701c12780c25dbeedc05da717c686
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-20 16:21:23 -0500

    fix: update lambda return value

--- src/functions/update/index.js
+++ src/functions/update/index.js
@@ -1,31 +1,34 @@
 import AWS from 'aws-sdk'
 const db = new AWS.DynamoDB.DocumentClient()
 
-async function updateItem(id, updatedData) {
-  const params = {
-    TableName: process.env.TABLE_NAME,
-    Key: { id },
-    UpdateExpression: 'set #d = :d',
-    ExpressionAtributeNames: { '#d': data },
-    ExpressionAttributeValues: { ':d': updatedData },
-  }
-  try {
-    await db.update(params).promise()
-  } catch (err) {
-    return err
-  }
-}
+// async function updateItem(id, updatedData) {
+//   const params = {
+//     TableName: process.env.TABLE_NAME,
+//     Key: { id },
+//     UpdateExpression: 'set #d = :d',
+//     ExpressionAtributeNames: { '#d': data },
+//     ExpressionAttributeValues: { ':d': updatedData },
+//   }
+//   try {
+//     await db.update(params).promise()
+//   } catch (err) {
+//     return err
+//   }
+// }
 
 export default async (event) => {
   const requestJSON = JSON.parse(event.body)
   const id = event.pathParameters.id
-  const { data } = requestJSON
 
   try {
-    console.log(`id is: ${id}`)
-    console.log(`data is :${data}`)
-    await updateItem(id, data)
-    return `item with id: ${id} updated with data: ${data}`
+    await db.update({
+      TableName: process.env.TABLE_NAME,
+      Key: { id },
+      UpdateExpression: 'set #d = :d',
+      ExpressionAtributeNames: { '#d': data },
+      ExpressionAttributeValues: { ':d': requestJSON.data },
+    })
+    return `item with id: ${id} updated with data: ${requestJSON.data}`
   } catch (err) {
     return { error: err }
   }
