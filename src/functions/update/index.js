be9b5c23ff74eca679dc82a594408966cac9c7af
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-21 01:53:26 -0500

    fix: update lambda return value

--- src/functions/update/index.js
+++ src/functions/update/index.js
@@ -1,27 +1,32 @@
 import AWS from 'aws-sdk'
 const db = new AWS.DynamoDB.DocumentClient()
 
-async function updateItem(key, updatedData) {
+// async function updateItem(key, updatedData) {
+//   const params = {
+//     TableName: process.env.TABLE_NAME,
+//     Key: { id: key },
+//     UpdateExpression: 'set #d = :d',
+//     ExpressionAttributeNames: { '#d': data },
+//     ExpressionAttributeValues: { ':d': `${updatedData}` },
+//   }
+//   try {
+//     await db.update(params).promise()
+//   } catch (err) {
+//     return err
+//   }
+// }
+
+export default async (event) => {
   const params = {
     TableName: process.env.TABLE_NAME,
-    Key: { id: key },
+    Key: { id: event.pathParameters.id },
     UpdateExpression: 'set #d = :d',
     ExpressionAttributeNames: { '#d': data },
-    ExpressionAttributeValues: { ':d': `${updatedData}` },
+    ExpressionAttributeValues: { ':d': event.body.data },
   }
   try {
     await db.update(params).promise()
-  } catch (err) {
-    return err
-  }
-}
-
-export default async (event) => {
-  const data = JSON.parse(JSON.stringify(event.body)).data
-  try {
-    console.log(data)
-    await updateItem(event.pathParameters.id, data)
-    return 'updated successfully'
+    return `${event.pathParameters.id} item updated with data: ${event.body.data}`
   } catch (err) {
     return { error: err }
   }
