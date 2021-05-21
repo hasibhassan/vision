e6b1ed259b319cf68b41823585eb197efa97bcbe
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-21 01:18:33 -0500

    fix: update lambda helper function

--- src/functions/update/index.js
+++ src/functions/update/index.js
@@ -1,32 +1,24 @@
 import AWS from 'aws-sdk'
 const db = new AWS.DynamoDB.DocumentClient()
 
-// async function updateItem(key, updatedData) {
-//   const params = {
-//     TableName: process.env.TABLE_NAME,
-//     Key: { id: key },
-//     UpdateExpression: 'set #d = :d',
-//     ExpressionAtributeNames: { '#d': data },
-//     ExpressionAttributeValues: { ':d': updatedData },
-//   }
-//   try {
-//     await db.update(params).promise()
-//   } catch (err) {
-//     return err
-//   }
-// }
+async function updateItem(key, updatedData) {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: { id: key },
+    UpdateExpression: 'set #d = :d',
+    ExpressionAttributeNames: { '#d': data },
+    ExpressionAttributeValues: { ':d': updatedData },
+  }
+  try {
+    await db.update(params).promise()
+  } catch (err) {
+    return err
+  }
+}
 
 export default async (event) => {
   try {
-    await db
-      .update({
-        TableName: process.env.TABLE_NAME,
-        Key: { id: event.pathParameters.id },
-        UpdateExpression: 'set #d = :d',
-        ExpressionAttributeNames: { '#d': data },
-        ExpressionAttributeValues: { ':d': event.body.data },
-      })
-      .promise()
+    await updateItem(event.pathParameters.id, event.body.data)
     console.log(event.pathParameters.id)
     console.log(event.body)
     return `updated successfully`
