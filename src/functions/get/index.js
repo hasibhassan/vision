60ec5d63f9badebf1b6c89b5982c41b3cb4be52e
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-29 23:48:50 -0500

    fix: get lambda doc client params

--- src/functions/get/index.js
+++ src/functions/get/index.js
@@ -1,10 +1,10 @@
 import AWS from 'aws-sdk'
 const db = new AWS.DynamoDB.DocumentClient()
 
-async function getItem(PK, SK) {
+async function getItem(PK) {
   const params = {
     TableName: process.env.TABLE_NAME,
-    Key: { PK, SK },
+    Key: { PK },
   }
   try {
     const item = await db.get(params).promise()
@@ -16,7 +16,7 @@ async function getItem(PK, SK) {
 
 export default async (event) => {
   try {
-    let result = await getItem(event.pathParameters.id, event.pathParameters.id)
+    let result = await getItem(event.pathParameters.id)
     result = JSON.stringify(result)
     console.log(result)
     return result
