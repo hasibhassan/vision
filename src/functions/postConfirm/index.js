9b096f4587adb148bc15a5ac65f1c1398861d11c
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-06-17 11:47:43 -0500

    fix: postConfirm handler params

--- src/functions/postConfirm/index.js
+++ src/functions/postConfirm/index.js
@@ -5,63 +5,26 @@ async function addUserToDB(event, date) {
   const params = {
     TableName: process.env.TABLE_NAME,
     Item: {
-      PK: event.request.userAttributes.sub,
-      // `USER#${event.request.userAttributes.sub}`,
-      SK: event.request.userAttributes.sub,
-      // `USER#${event.request.userAttributes.sub}`,
-      // EMAIL: `EMAIL#${event.request.userAttributes.email}`,
-      // CREATEDAT: date.toISOString(),
+      PK: `USER#${event.request.userAttributes.sub}`,
+      SK: `USER#${event.request.userAttributes.sub}`,
+      EMAIL: `EMAIL#${event.request.userAttributes.email}`,
+      CREATEDAT: date.toISOString(),
     },
   }
 
   try {
-    console.log('adding user to db now... lets see what happens')
-    const data = await db.put(params).promise()
-    console.log(`Successfully added data ${JSON.stringify(data)} to DB`)
-    return data
+    await db.put(params).promise()
   } catch (err) {
     return { error: err }
   }
 }
 
 export default async (event) => {
-  console.log(
-    `event.request.userAttributes is :${JSON.stringify(
-      event.request.userAttributes.sub
-    )}, email is: ${JSON.stringify(
-      event.request.userAttributes.email
-    )} at least it got to in the handler.`
-  )
-  // const date = new Date()
+  const date = new Date()
   try {
-    console.log(`now its in the try block in the handler.`)
-    const user = await addUserToDB(event)
-    console.log(`done adding user: ${JSON.stringify(user)} to dynamoDB`)
+    await addUserToDB(event, date)
     return event
   } catch (err) {
     return { error: err }
   }
 }
-
-// async function createItem(itemData) {
-//   var params = {
-//     TableName: 'ProductTable',
-//     Item: itemData,
-//   }
-//   try {
-//     await docClient.put(params).promise()
-//   } catch (err) {
-//     return err
-//   }
-// }
-
-// // usage
-// exports.handler = async (event, context) => {
-//   try {
-//     const { data } = event.body
-//     await createItem(data)
-//     return { body: 'successfully created item' }
-//   } catch (err) {
-//     return { error: err }
-//   }
-// }
