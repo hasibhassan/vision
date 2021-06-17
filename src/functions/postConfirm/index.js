b4756c9944904803ecdc15d0dd8641da45e8871f
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-06-16 23:58:12 -0500

    fix: postConfirm handler

--- src/functions/postConfirm/index.js
+++ src/functions/postConfirm/index.js
@@ -2,34 +2,34 @@ import AWS from 'aws-sdk'
 const db = new AWS.DynamoDB.DocumentClient()
 
 async function addUserToDB(event, date) {
-  if (event.request.userAttributes.sub) {
-    const params = {
-      TableName: process.env.TABLE_NAME,
-      Item: {
-        PK: `USER#${event.request.userAttributes.sub}`,
-        SK: `USER#${event.request.userAttributes.sub}`,
-        EMAIL: `EMAIL#${event.request.userAttributes.email}`,
-        CREATEDAT: date.toISOString(),
-      },
-    }
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      PK: `USER#${event.sub}`,
+      SK: `USER#${event.sub}`,
+      EMAIL: `EMAIL#${event.email}`,
+      CREATEDAT: date.toISOString(),
+    },
+  }
 
-    try {
-      await db.put(params).promise()
-      console.log('Successfully added user to DB')
-    } catch (err) {
-      return err
-    }
+  try {
+    await db.put(params).promise()
+    console.log('Successfully added user to DB')
+  } catch (err) {
+    return err
   }
 }
 
 export default async (event) => {
+  const requestJSON = JSON.parse(event.request.userAttributes)
   console.log(
-    `event.request.userAttributes is :${event.request.userAttributes}at least it got to in the handler.`
+    `event.request.userAttributes is :${requestJSON.sub}, email is: ${requestJSON.email} at least it got to in the handler.`
   )
   const date = new Date()
   try {
     console.log(`now its in the try block in the handler.`)
-    await addUserToDB(event, date)
+    await addUserToDB(JSON.parse, date)
+    console.log('done adding user to dynamoDB')
     return event
   } catch (err) {
     return { error: err }
