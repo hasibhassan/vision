df9fa69ca264fe69c73cbd6a5fc62bd19cff9d10
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-10-15 03:07:28 -0500

    refactor: add amplify exports to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/reset.css'
+import Amplify from 'aws-amplify'
+import config from '../src/aws-exports'
 import { QueryClient, QueryClientProvider } from 'react-query'
+
+Amplify.configure({ ...config })
 
 function MyApp({ Component, pageProps }) {
   const queryClient = new QueryClient()
