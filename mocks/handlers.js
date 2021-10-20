7911acced9e18333ed3af4fa9d1f33877ad7f1aa
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-10-20 03:43:01 -0500

    refactor: add comments in msw handler file

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,6 +1,7 @@
 import { rest } from 'msw'
 
 export const handlers = [
+  // TODO #17 fix mock service worker handlers
   rest.get('/user', (req, res, ctx) => {
     return res(ctx.json({ username: 'admin' }))
   }),
