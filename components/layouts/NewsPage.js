a6b68d15ca2e0dd41d0a8782f79d9d31fd5d49e4
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-10-21 03:21:22 -0500

    refactor: add spinner on news page

--- components/layouts/NewsPage.js
+++ components/layouts/NewsPage.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 import NewsCards from '@ui/Cards/NewsCards'
+import Spinner from '@ui/Spinner/Spinner'
 
 export default function NewsPage() {
   const [dataArray, setDataArray] = useState([])
-  const url = 'https://api.blockchair.com/news?q=language(en)'
+  const [isLoading, setIsLoading] = useState(false)
 
   const isMobile = useMediaQuery({
     maxWidth: 1024,
@@ -13,13 +14,17 @@ export default function NewsPage() {
     minWidth: 1024,
   })
 
+  const url = 'https://api.blockchair.com/news?q=language(en)'
+
   useEffect(() => {
     const fetchNewsArticles = async () => {
+      setIsLoading(true)
       await fetch(url)
         .then((res) => res.json())
         .then((jsonData) => {
           setDataArray(Object.values(jsonData.data))
         })
+      setIsLoading(false)
     }
 
     fetchNewsArticles()
@@ -27,6 +32,7 @@ export default function NewsPage() {
 
   return (
     <div>
+      {isLoading && <Spinner />}
       {isMobile && <NewsCards dataArray={dataArray} />}
       {isDesktop &&
         dataArray.map((el) => (
--- components/ui/Spinner/Spinner.js
+++ components/ui/Spinner/Spinner.js
@@ -1,9 +1,12 @@
 import LoadingSpinner from '@commercetools-uikit/loading-spinner'
+import styles from './Spinner.module.css'
 
 export default function Spinner() {
   return (
     <div className={styles.center}>
-      <LoadingSpinner size="s">Loading</LoadingSpinner>
+      <LoadingSpinner size="s" maxDelayDuration={0}>
+        Loading
+      </LoadingSpinner>
     </div>
   )
 }
