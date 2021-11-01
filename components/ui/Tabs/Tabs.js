be99be6fbcfacf8d8810697739fb686ba83de5b7
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-11-01 16:35:39 -0500

    refactor: add tabs component

---
+++ components/ui/Tabs/Tabs.js
@@ -0,0 +1,54 @@
+import styles from './Tabs.module.css'
+import cx from 'classnames'
+
+const tabs = [
+  { name: 'My Account', href: '#', current: false },
+  { name: 'Company', href: '#', current: false },
+  { name: 'Team Members', href: '#', current: true },
+  { name: 'Billing', href: '#', current: false },
+]
+
+export default function Tabs() {
+  return (
+    <div>
+      <div className={styles.selectContainer}>
+        <label htmlFor="tabs" className={styles.srOnly}>
+          Select a tab
+        </label>
+        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+        <select
+          id="tabs"
+          name="tabs"
+          className={styles.select}
+          defaultValue={tabs.find((tab) => tab.current).name}
+        >
+          {tabs.map((tab) => (
+            <option key={tab.name}>{tab.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className={styles.tabsContainer}>
+        <div className={styles.navContainer}>
+          <nav className={styles.nav} aria-label="Tabs">
+            {tabs.map((tab) => (
+              <a
+                key={tab.name}
+                href={tab.href}
+                className={cx(
+                  styles.defaultTabs,
+                  {
+                    [styles.current]: tab.current,
+                  },
+                  { [styles.notCurrent]: !tab.current }
+                )}
+                aria-current={tab.current ? 'page' : undefined}
+              >
+                {tab.name}
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
