5b00af4077d07eee7381bb3e82e777e8d68fe68e
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-11-03 15:21:33 -0500

    refactor: update saved and portfolio tabs

--- components/sections/Profile/Portfolio.js
+++ components/sections/Profile/Portfolio.js
@@ -1,8 +1,12 @@
+import { useAppContext } from '@utils/Context/AppContext'
 import styles from './Portfolio.module.css'
 
 export default function Portfolio({ userData }) {
+  const { state } = useAppContext()
+  const { portfolio } = state
   return (
     <div>
+      {portfolio.length === 0 && <p>You have no coins in your portfolio</p>}
       <h1 className={styles.header}>Account Email is: {userData.user}</h1>
     </div>
   )
--- components/sections/Profile/Saved.js
+++ components/sections/Profile/Saved.js
@@ -1,9 +1,38 @@
+import { useAppContext } from '@utils/Context/AppContext'
 import styles from './Saved.module.css'
+import { FaRegThumbsUp } from 'react-icons/fa'
 
-export default function Saved({ userData }) {
+export default function Saved() {
+  const { state } = useAppContext()
+  const { likedNews } = state
   return (
-    <div>
-      <p className={styles.header}>Account Created At: {userData.createdAt}</p>
+    <div className={styles.outsideContainer}>
+      <div className={styles.rootContainer}>
+        <ul role="list" className={styles.ul}>
+          {likedNews.map((newsLink, idx) => (
+            <li key={idx}>
+              <div className={styles.listItemContainer}>
+                <div className={styles.listItem}>
+                  <div>
+                    <FaRegThumbsUp className={styles.likeIcon} />
+                  </div>
+                  <div className={styles.linkContainer}>
+                    <div>
+                      <a
+                        href={newsLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <p className={styles.link}>{newsLink}</p>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
