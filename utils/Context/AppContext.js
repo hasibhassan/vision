47d062701522872920190eed247718f71a4851f9
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-11-02 19:49:48 -0500

    refactor: update server state context

--- utils/Context/AppContext.js
+++ utils/Context/AppContext.js
@@ -18,23 +18,50 @@ export function AppContextWrapper({ children }) {
     return { state, dispatch }
   }, [state, dispatch])
 
-  const [sessionStorageValue, setSessionStorageValue] = useSessionStorage(
-    'visionState',
-    state
-  )
+  // const [sessionStorageValue, setSessionStorageValue] = useSessionStorage(
+  //   'visionState',
+  //   state
+  // )
+
+  const checkIsAuth = async () => {
+    try {
+      const { username } = await Auth.currentAuthenticatedUser()
+      if (username) {
+        return true
+      }
+    } catch (err) {
+      return false
+    }
+  }
 
   useEffect(() => {
-    // If there's already state in sessionStorage update the app state store w/ the sessionStorage state
-    if (sessionStorageValue) {
-      dispatch({ type: 'init_stored', value: sessionStorageValue })
+    // Get the server state and initialize the local state store with that if the user is signed in
+
+    const initServerState = async () => {
+      const { username: email } = await Auth.currentAuthenticatedUser()
+      const response = await API.get('visionapi', `/users/${email}`, {})
+      const userItem = response.Item.contextstate
+      const serverState = JSON.parse(userItem)
+      console.log('the state retreived from the server is:', serverState)
+      dispatch({ type: 'init_stored', value: serverState })
     }
+    const isAuth = checkIsAuth()
+
+    if (isAuth) {
+      initServerState()
+    }
+
+    // If there's already state in sessionStorage update the app state store w/ the sessionStorage state
+    // if (sessionStorageValue) {
+    //   dispatch({ type: 'init_stored', value: sessionStorageValue })
+    // }
   }, [])
 
   useEffect(() => {
     // Create and/or set a new sessionStorage key named 'state'
-    if (state !== initialState) {
-      setSessionStorageValue(state)
-    }
+    // if (state !== initialState) {
+    //   setSessionStorageValue(state)
+    // }
 
     // Update DynamoDB user item with the current state object
     const updateDB = async (state) => {
@@ -54,7 +81,12 @@ export function AppContextWrapper({ children }) {
       }
     }
 
-    updateDB(state)
+    // If the user is signed in then update the server state on every state change
+    const isAuth = checkIsAuth()
+
+    if (isAuth) {
+      updateDB(state)
+    }
   }, [state])
 
   return (
