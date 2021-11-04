8ab9665832b7f3e3a58b9757a8efb18452b4c994
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2021-11-03 20:09:34 -0500

    refactor: fix useGetCardData hook

---
+++ components/ui/Buttons/PortfolioLikeButton.js
@@ -0,0 +1,49 @@
+import heart from 'react-useanimations/lib/heart'
+import UseAnimations from 'react-useanimations'
+import { useAppContext } from '@utils/Context/AppContext'
+import { Auth } from 'aws-amplify'
+import { toast } from 'react-toastify'
+
+export default function PortfolioLikeButton({
+  size = 24,
+  color = 'red',
+  newsHash,
+}) {
+  const { state, dispatch } = useAppContext()
+  const { likedNews } = state
+  const isLiked = likedNews.includes(newsHash)
+
+  const checkIsAuth = async () => {
+    try {
+      await Auth.currentAuthenticatedUser()
+      return true
+    } catch (err) {
+      return false
+    }
+  }
+
+  const setIsLiked = async (hash) => {
+    const isAuth = await checkIsAuth()
+
+    if (isAuth) {
+      if (likedNews.includes(hash)) {
+        dispatch({ type: 'unlike_news', value: hash })
+      } else {
+        dispatch({ type: 'like_news', value: hash })
+      }
+    } else {
+      toast('You must be logged in to do that', { type: 'error' })
+    }
+  }
+
+  return (
+    <UseAnimations
+      animation={heart}
+      size={size}
+      onClick={() => setIsLiked(newsHash)}
+      reverse={isLiked}
+      strokeColor={color}
+      pathCss={`fill:${color}`}
+    />
+  )
+}
--- utils/useGetCardData.js
+++ utils/useGetCardData.js
@@ -5,7 +5,7 @@ const useGetCardData = (options, number = 25) => {
     `cryptoCards`,
     async () => {
       const response = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page={number}&page=1&sparkline=false`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${number}&page=1&sparkline=false`
       )
       return await response.json()
     },
